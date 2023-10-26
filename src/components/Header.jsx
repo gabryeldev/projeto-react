d7147d6d0ade50afa26916abd34b20b7677f025a
gabryeldev <gabryel.alves@araujo.com.br>
2023-10-26 00:45:54 -0300

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,35 +16,29 @@ function Header() {
     return (
         <nav className="navbar">
             <a href="#"> <img src="src/assets/img/logo.png" className="Logo" alt="Logo"/></a>
-            
-            <div>
-                <ul className={isClicked ? 'active' : ''}>
-                    <li>
-                        <div className="search-box">
-                            <input type="text" placeholder="Search anything" className="search-input"/>
-                            <i className="fas fa-search search-btn"></i>
+            <div className='container'>
+                <div className='cart-wrapper'>
+                        <div className="cart" onClick={adicionarAoCarrinho}>
+                            <ion-icon className="ion-icon" name="cart-outline"></ion-icon>
                         </div>
-                    </li>
-                    <li><a className="active" href="#">Insira seu <br/> CEP </a></li>
-                    <li><a className="active" href="#">Olá, faça login <br/> ou cadastre-se</a></li>
-                    
-                    <li className="lg-bag">
-                        <a href="#" onClick={adicionarAoCarrinho}><i className="fa fa-shopping-bag"></i></a>
-                    </li>
-                    <li>
-                        <div className='cart-wrapper'>
-                            <div className="cart">
-                                <ion-icon name="cart-outline"></ion-icon>
+                        <div className={cartCount > 0 ? "cart-badge ion-icon" : "none"}><span id="cart-count">{cartCount}</span></div>
+                </div>
+                <div>
+                    <ul className={isClicked ? 'active' : ''}>
+                        <li>
+                            <div className="search-box">
+                                <input type="text" placeholder="Search anything" className="search-input"/>
+                                <i className="fas fa-search search-btn"></i>
                             </div>
-                            <div className={cartCount > 0 ? "cart-badge" : "none"}><span id="cart-count">{cartCount}</span></div>
-                        </div>
-                    </li>
-                    <a className="close" ><i className="far fa-times" onClick={abrirFecharMenu}></i></a>
-                </ul>
-            </div>
-            <div className="mobile">
-                <i className="bar fas fa-outdent" onClick={abrirFecharMenu}></i>
-                <a className="active" href="#" ><i className="fa fa-shopping-bag" ></i></a>
+                        </li>
+                        <li><a className="active" href="#">Insira seu <br/> CEP </a></li>
+                        <li><a className="active" href="#">Olá, faça login <br/> ou cadastre-se</a></li>
+                        <a className="close" ><i className="far fa-times" onClick={abrirFecharMenu}></i></a>
+                    </ul>
+                </div>
+                <div className="mobile">
+                    <i className="bar fas fa-outdent" onClick={abrirFecharMenu}></i>
+                </div>
             </div>
         </nav>
     )
