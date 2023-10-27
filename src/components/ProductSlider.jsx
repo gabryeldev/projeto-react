0d08ca19983a85d0da13a7551d4a13bf7eb1eb39
gabryeldev <gabryel.alves@araujo.com.br>
2023-10-27 00:33:53 -0300

    up

--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -7,15 +7,15 @@ function ProductSlider( parametros ){
         dots: true,
         infinite: false,
         speed: 300,
-        slidesToShow: 4,
+        slidesToShow: 5,
         slidesToScroll: 1,
         arrows : false,
         responsive: [
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 4,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
               }
@@ -23,14 +23,14 @@ function ProductSlider( parametros ){
             {
               breakpoint: 600,
               settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 3,
+                slidesToScroll: 1
               }
             },
             {
               breakpoint: 480,
               settings: {
-                slidesToShow: 1,
+                slidesToShow: 2,
                 slidesToScroll: 1
               }
             }
